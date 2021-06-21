90d6ae0a1e1053142d325a0a348684070c3ca014
Cari <t.carina@rocketmail.com>
2021-06-21 00:14:35 -0400

    heroku update

--- models/transactions.js
+++ models/transactions.js
@@ -1,11 +1,1 @@
-module.exports = [{
-    date: '01/01/20',
-    name: '',
-    amount: 100,
-    from: 'testing'
-}, {
-    date: '02/0/22',
-    name: '',
-    amount: 200,
-    from: 'testing2'
-}]
+module.exports = []
---
+++ readme.md
@@ -0,0 +1,4 @@
+react repo : https://github.com/tCarina/budgeting-app-project-prompt
+react site : https://budgetappfrontend.netlify.app
+
+express site : https://pure-earth-58108.herokuapp.com/
