1bd5c9c7f4debc969c162d1d41a2723b66defd92
Cari <t.carina@rocketmail.com>
2021-06-21 19:57:05 -0400

    fix dummy data

--- models/transactions.js
+++ models/transactions.js
@@ -1,36 +1,36 @@
 module.exports = [{
     name: 'Direct Deposit',
-    date: 2021-06-01,
+    date: '2021-06-01',
     amount: 2000,
     from: 'Work'
 }, {
     name: 'Sephora',
-    date: 2021-06-01,
+    date: '2021-06-01',
     amount: -150,
     from: 'Checking'
 }, {
     name: 'Starbucks',
-    date: 2021-06-03,
+    date: '2021-06-03',
     amount: -12,
     from: 'Checking',
 }, {
     name: 'Sephora Refund',
-    date: 2021-06-05,
+    date: '2021-06-05',
     amount: 30,
     from: 'refund'
 }, {
     name: 'Dinner',
-    date: 2021-06-10,
+    date: '2021-06-10',
     amount: 180,
     from: 'checking'
 }, {
     name: 'Phone Bill',
-    date: 2021-06-14,
+    date: '2021-06-14',
     amount: 80,
     from: 'checking'
 }, {
     name: 'UberEats',
-    date: 2021-06-19,
+    date: '2021-06-19',
     amount: 42,
     from: 'checking'
 
